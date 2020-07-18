cd7a94c0a3f32d614d9ae15de4918cf77a886211
vksinghh25 <vipkumar@expedia.com>
2020-07-19 03:15:47 +0530

    bye controller

---
+++ src/main/java/com/example/greeting/ByeController.java
@@ -0,0 +1,13 @@
+package com.example.greeting;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ByeController {
+
+    @GetMapping("/bye")
+    public String bye() {
+        return "Bye!";
+    }
+}
