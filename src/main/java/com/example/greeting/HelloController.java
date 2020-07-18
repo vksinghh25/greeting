3f731781866f76a4f2296183ddb2361a217d8f0a
vksinghh25 <vipkumar@expedia.com>
2020-07-19 00:15:05 +0530

    howdy controller

--- src/main/java/com/example/greeting/HelloController.java
+++ src/main/java/com/example/greeting/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String hello() {
         return "Hello World!";
     }
+
+    @GetMapping("/howdy")
+    public String howdy() {
+        return "Howdy!";
+    }
 }
