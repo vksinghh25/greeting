02c3b521e3dab8834820a2070b5d426e5296d4b0
vksinghh25 <vipkumar@expedia.com>
2020-07-22 10:50:24 +0530

    pushing HelloController

--- src/main/java/com/example/greeting/HelloController.java
+++ src/main/java/com/example/greeting/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello Hello World!";
+        return "Hello World!";
     }
 
     @GetMapping("/howdy")
